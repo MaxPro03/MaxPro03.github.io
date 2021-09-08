f34c8de45d5c9980985517660651f03597ea8102
Max <68842645+MaxPro03@users.noreply.github.com>
2021-09-09 01:28:55 +0500

    Add files via upload

---
+++ NewProject/assets/scripts/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    //Main slider
+    $('.main-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
+
+});
